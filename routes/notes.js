2187c577bccbf37bac838f55e4f4db32640f6d11
Darx <mcrivabode@gmail.com>
2022-10-28 16:45:24 -0700

    setting up notes.js for getting and retrieving information

---
+++ routes/notes.js
@@ -0,0 +1,3 @@
+const fs = require('fs') // node.js built-in function to manage files
+const notes = require('express').Router(); // uses express modulation
+const { v4: uuidv4 } = require('uuid'); // creates crypotgraphically strong random values
