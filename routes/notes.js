9c67ecdee5c44e9f57486c1d7a4837545f06722c
Darx <mcrivabode@gmail.com>
2022-10-28 16:51:42 -0700

    add notes.get functionality to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,13 @@
 const fs = require('fs') // node.js built-in function to manage files
 const notes = require('express').Router(); // uses express modulation
 const { v4: uuidv4 } = require('uuid'); // creates crypotgraphically strong random values
+
+notes.get('/', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) { // checks to see if response returns an error first
+            console.log(err); // logs the error
+        } else {
+            res.json(JSON.parse(data)) // parses out data retrieved from db.json
+        }
+    })
+});
