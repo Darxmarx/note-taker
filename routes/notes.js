92adedd892b851e5d47b58070671d0cde2da0711
Darx <mcrivabode@gmail.com>
2022-10-28 17:54:06 -0700

    fix error when posting notes in notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -9,6 +9,7 @@ notes.get('/', (req, res) => {
             console.log(err); // logs the error
         } else {
             res.json(JSON.parse(data)) // parses out data retrieved from db.json
+            console.log("Successfully retrieved saved note.");
         }
     })
 });
@@ -19,36 +20,41 @@ notes.post('/', (req, res) => {
     const { title, text } = req.body;
 
     // if note has both a title and text associated
-    if(title && text) {
+    if (title && text) {
         // stores the new note's title, text, and unique id that will be used for deletion later
         const newNote = {
             title,
             text,
             id: uuidv4(), // function installed from uuid that randomly generates a string
         };
+
+
+        // function that modifies the db.json file
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) { // checks to see if response returns an error first
+                console.log(err); // logs the error
+            } else {
+                // takes data read from db.json, parses it, and appends the new note as a brand new object
+                const parsedDB = JSON.parse(data);
+                parsedDB.push(newNote);
+
+                //writes the modified data to the db.json file
+                fs.writeFile(
+                    './db/db.json', // writes to the file
+                    JSON.stringify(parsedDB, null, 2), // stringifies the parsed object back into JSON, null allows all properties of the object to be included in the string, and 2 represents the amount of spaces included for legibility in the new code
+                    (err, data) => {
+                        if (err) { // checks error first
+                            console.log(err);
+                        } else {
+                            res.json(data);
+                            console.log('Successfully added new tip.');
+                        }
+                    });
+
+            }
+        });
     }
-
-    // function that modifies the db.json file
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if(err) { // checks to see if response returns an error first
-            console.log(err); // logs the error
-        } else {
-            // takes data read from db.json, parses it, and appends the new note as a brand new object
-            const parsedDB = JSON.parse(data);
-            parsedDB.push(newNote);
-
-            //writes the modified data to the db.json file
-            fs.writeFile(
-                './db/db.json', // writes to the file
-                JSON.stringify(parsedDB, null, 2), // stringifies the parsed object back into JSON, null allows all properties of the object to be included in the string, and 2 represents the amount of spaces included for legibility in the new code
-                (err, data) => {
-                    if(err) { // checks error first
-                        console.log(err);
-                    } else {
-                        res.json(parsedDB);
-                        console.log ('Successfully added new tip.');
-                    }
-                });
-        }
-    });
 });
+
+// export this file's code for usage in the app
+module.exports = notes;
