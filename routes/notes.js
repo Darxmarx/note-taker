03a902e62f736aaf9c275d9d892819a8740daf3f
Darx <mcrivabode@gmail.com>
2022-10-28 17:00:11 -0700

    set up what data is required to store new note, give new note unique id

--- routes/notes.js
+++ routes/notes.js
@@ -2,6 +2,7 @@ const fs = require('fs') // node.js built-in function to manage files
 const notes = require('express').Router(); // uses express modulation
 const { v4: uuidv4 } = require('uuid'); // creates crypotgraphically strong random values
 
+// GET route to retrieve notes data
 notes.get('/', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) { // checks to see if response returns an error first
@@ -11,3 +12,21 @@ notes.get('/', (req, res) => {
         }
     })
 });
+
+// POST route to take user's inputted note
+notes.post('/', (req, res) => {
+    // properties taken from the db.json that form the body of user's input
+    const { title, text } = req.body;
+
+    // if note has both a title and text associated
+    if(title && text) {
+        // stores the new note's title, text, and unique id that will be used for deletion later
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+    }
+
+    
+})
